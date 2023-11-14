f6fc59ce588eee999c42698eb4d668d3c4e05751
Mahavir Nahata 1BM21CS100 <117442487+Viru12345@users.noreply.github.com>
2023-11-14 18:26:38 +0530

    Create grade.py

---
+++ grade.py
@@ -0,0 +1,22 @@
+marks=int(input("Enter ur grade: "))
+print(marks)
+if marks>=81 and marks<=100:
+    print("Very good")
+elif marks>=61 and marks<=80:
+    print("Good")
+elif marks>=41 and marks<=60:
+    print("Average")
+else:
+    print("fail")
+
+
+output:
+Enter ur grade: 100
+100
+Very good
+
+Enter ur grade: 41
+41
+Average
+
+
